7a3a0e917766e143e39d4d35adedde710d47f569
shivamanand <shivam_anand@outlook.com>
2020-11-05 01:18:04 +0530

    modified

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,13 +7,22 @@ const sideChangeRight = () => {
   document.getElementById("col-1").style.zIndex = "-1";
   document.getElementById("col-2").style.zIndex = "1";
   document.getElementById("col-2").style.background = "rgb(250, 75, 75)";
+  document.getElementById("arrowContainer2").classList.add("arrowContainer");
+  document.getElementById("arrowLeft").classList.add("arrowLeft");
+  document.getElementById("active-col-2").style.display = "none";
+  document.getElementById("hide").classList.remove("hidden");
 };
 
 const sideChangeLeft = () => {
   document.getElementById("col-1").style.transform = "translate3d(0%, 0, 0)";
-  document.getElementById("col-2").style.transform = "translate3d(-100%, 0, 0)";
+
   document.getElementById("col-1").style.zIndex = "1";
   document.getElementById("col-2").style.zIndex = "-1";
+  document.getElementById("col-2").style.background = "#fff";
+  document.getElementById("arrowContainer2").classList.remove("arrowContainer");
+  document.getElementById("arrowLeft").classList.remove("arrowLeft");
+  document.getElementById("active-col-2").style.display = "block";
+  document.getElementById("hide").classList.add("hidden");
 };
 
 arrowContainer.addEventListener("click", sideChangeRight, true);
